982552ab832398fdb80abd35c60fd18cc1b327de
davidkatz <15Dkatz@shcp.edu>
2016-05-24 19:56:12 -0700

    Created modal

--- www/js/events.js
+++ www/js/events.js
@@ -2,12 +2,13 @@ pardeezApp.controller('EventsCtrl', ["$scope", "$rootScope", "$ionicModal", "Eve
 function($scope, $rootScope, $ionicModal, Events) {
   $scope.events = Events;
 
-  $ionicModal.fromTemplateUrl('templates/addEvent-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  })
+  $ionicModal.fromTemplateUrl('../templates/addEvent-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up',
+   }).then(function(modal) {
+      $scope.modal = modal;
+   });
+
 
   $scope.addEvent = function() {
     console.log($rootScope.displayName, "displayName");
@@ -21,11 +22,28 @@ function($scope, $rootScope, $ionicModal, Events) {
   };
 
   $scope.openModal = function() {
-    console.log("attempting to openModal");
+    // console.log("attempting to openModal");
     $scope.modal.show();
   };
 
   // cancel button to close modal
-  
+  $scope.closeModal = function() {
+      $scope.modal.hide();
+  };
+
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
 
 }]);
